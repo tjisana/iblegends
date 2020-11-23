c9b32d75c721e635219df472b8fa5dcecb2bd023
Tjisana Kerr <tjisanakerr@Tjisanas-MBP.fios-router.home>
2020-11-23 17:39:53 -0500

    fixed script. Still ignorant on how shell scripts work. There was a path issue when I ran script from a folder that was not the script folder

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,8 @@
-source ../../venv/bin/activate
-python ../epl_fantasy/manage.py flush --noinput
-python ../epl_fantasy/manage.py generate_initial_fixtures
-python ../epl_fantasy/manage.py loaddata players_fixture
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source ${DIR}/../../venv/bin/activate
+python ${DIR}/../epl_fantasy/manage.py flush --noinput
+python ${DIR}/../epl_fantasy/manage.py generate_initial_fixtures
+python ${DIR}/../epl_fantasy/manage.py loaddata players_fixture
