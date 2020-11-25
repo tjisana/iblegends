a979e7d860f99c0603bf9930026c923409c65171
Tjisana Kerr <tjisanakerr@Tjisanas-MBP.fios-router.home>
2020-11-25 03:47:29 -0500

    what if someone doesn't use the website for more than a week

--- epl_fantasy/app/views.py
+++ epl_fantasy/app/views.py
@@ -9,11 +9,17 @@ import requests
 def index(request):
     current_event, points_are_final = get_current_event_and_status_from_web()
     all_players = Player.objects.all()
+    latest_week_in_db = Points.objects.last().week
     
     if not points_are_final:
-        Points.objects.filter(week=current_event).delete()
+        Points.objects.filter(week=current_event).delete()    
     
-    if not(current_event == Points.objects.last().week and points_are_final):
+    if (current_event - latest_week_in_db) > 1:
+        print([week for week in range(latest_week_in_db+1, current_event)])
+        for week in range(latest_week_in_db+1, current_event):
+            update_points_table_from_web(week, True)
+
+    if not(current_event == latest_week_in_db and points_are_final):
         update_points_table_from_web(current_event, points_are_final)
 
 
