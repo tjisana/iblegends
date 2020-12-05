d8e51b26c85f405dead5b0b54a63e533609ccc52
Tjisana Kerr <tjisana@gmail.com>
2020-12-05 14:59:59 -0500

    decided to redirect the url to epl fantasy with the idea this will be used for other projects and for other years outside of 2020

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', RedirectView.as_view(url='/eplfantasy/2020/', permanent=False), name='index'),
+    path('eplfantasy/2020/', views.index, name='eplfantasy20'),
 ]
